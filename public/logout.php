351bce7131ce5d669eeb3d71be5473fe65764c18
jubyte <julia_ferreira3@estudante.sesisenai.org.br>
2026-06-03 08:30:43 -0300

    docs -documentei o encerramento da sessão

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,13 @@
 <?php
-    session_start();
-    session_destroy();
-    header("Location: ../index.php");
-    exit();
+
+   // Inicia sessão atual
+   session_start();
+
+   // Remove todos os dados da sessão
+   session_destroy();
+
+   // Retorna para tela de login
+   header("Location: ../index.php");
+   exit();
+
 ?>
